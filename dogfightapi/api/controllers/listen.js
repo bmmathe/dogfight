5066d2565fb65e0ce86e36d92e29d67fce946b1e
Brett Mathe <brett.mathe@gmail.com>
2017-02-21 12:30:38 -0500

    Fixed bug in listen controller when adding actions.

--- dogfightapi/api/controllers/listen.js
+++ dogfightapi/api/controllers/listen.js
@@ -30,11 +30,11 @@ function listen(req, res) {
     } else if(command == "getdog") {
         var dogName = commands[2];
         redis.getDog(dogName, function(err, dog) {
-            res.json({text: '```'+JSON.stringify(dog)+'```'});
+            res.json({text: '```'+JSON.stringify(dog, null, 2)+'```'});
         });        
     } else if(command == "add") {
         var type = commands[2];
-        var actions = request.split('"');
+        var actions = request.text.split('"');
         var name = commands[3];
         var message = actions[1];
         var value = parseInt(actions[2]);
